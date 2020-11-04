b7dbc565cc3a35449f415f5169cad53e54a7e42a
SebasBar <toolt2y@gmail.com>
2020-11-04 12:26:06 +0100

    a small update

--- src/components/CategoriesBar.js
+++ src/components/CategoriesBar.js
@@ -1,65 +1,65 @@
-// import React, { useState } from "react";
-// import SearchBar from './SearchBar';
+import React from "react";
 
-// class CreateCategoriesBar extends React.Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//         userInput: ""
-//     }
-// }
-// handleSubmit = (event) => {
-//     event.preventDefault();
-//     this.props.onSearch(this.state.userInput)
 
-// }
-// handleChange = (event) => {
-//     this.setState({ userInput:event.target.value });
-// }
+class CreateCategoriesBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        categoryInput: ""
+    }
+}
+handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSearch(this.state.categoryInput)
+
+}
+handleChange = (event) => {
+    this.setState({ categoryInput:event });
+}
  
-//   render(){
-//     return(
-//       <form onSubmit={(event)=>this.handleSubmit(event)}>
-//     <div className="categories-bar">
-//     <button
-//       id="technology"
-//         onClick = {() => SearchBar('technology')}
-//       >Technology</button>
-//         <button
-//       id="fashion"
-//         onClick = {() => searchButton('fashion')}
-//       >Fashion</button>
-//         <button
-//       id="health-beauty"
-//         onClick = {() => searchButton('health&beauty')}
-//       >Health and Beauty</button>
-//         <button
-//       id="motors"
-//         onClick = {() => searchButton('motors')}
-//       >Motors</button>
-//         <button
-//       id="collectibles"
-//         onClick = {() => searchButton('collectibles')}
-//       >Collectibles</button>
-//         <button
-//       id="industrial"
-//         onClick = {() => searchButton('industrial')}
-//       >Industrial</button>
-//         <button
-//       id="sports"
-//         onClick = {() => searchButton('sports')}
-//       >Sports</button>
-//         <button
-//       id="homeandgarden"
-//         onClick = {() => searchButton('home&garden')}
-//       >Home and Garden</button>
-//       <Product properties={products} />
-//     </div>
-//     </form>
-//   );
-// }
-// }
+  render(){
+    return(
+      <form onSubmit={()=>this.handleSubmit(this.id)}>
+    <div className="categories-bar">
+    <input
+        type="submit"
+        id="technology"
+        value = "Technology"
+     />
+        {/* <button
+      id="fashion"
+        onClick = {() => searchButton('fashion')}
+      >Fashion</button>
+        <button
+      id="health-beauty"
+        onClick = {() => searchButton('health&beauty')}
+      >Health and Beauty</button>
+        <button
+      id="motors"
+        onClick = {() => searchButton('motors')}
+      >Motors</button>
+        <button
+      id="collectibles"
+        onClick = {() => searchButton('collectibles')}
+      >Collectibles</button>
+        <button
+      id="industrial"
+        onClick = {() => searchButton('industrial')}
+      >Industrial</button>
+        <button
+      id="sports"
+        onClick = {() => searchButton('sports')}
+      >Sports</button>
+        <button
+      id="homeandgarden"
+        onClick = {() => searchButton('home&garden')}
+      >Home and Garden</button> */}
+    </div>
+    </form>
+  );
+}
+}
 
 
 
-// export default CreateCategoriesBar;
+export default CreateCategoriesBar;
