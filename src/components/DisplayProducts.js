76dc8e73c11d095841d6446044096b016d987582
SebasBar <toolt2y@gmail.com>
2020-11-18 11:51:24 +0100

    handle undefined error

--- src/components/DisplayProducts.js
+++ src/components/DisplayProducts.js
@@ -13,24 +13,23 @@ class DisplayProduct extends React.Component {
   render() {
     console.log("state", this.props.product);
     return (
-
-
       <div className="results-section">
-
-        {this.props.product.map((product, index) => (
-          <Card
-            price={product.sellingStatus[0].currentPrice[0].__value__}
-            image={product.galleryURL[0]}
-            title={product.title}
-            location={product.location[0]}
-            shipping={product.shippingInfo[0].shippingType[0]}
-            link={product.viewItemURL[0]}
-            country={product.country}
-            id={product.itemId}
-          />
-
-
-        ))}
+        {this.props.product === undefined ? (
+          <h1 style={{ color: "red" }}>Please enter a valid search</h1>
+        ) : (
+          this.props.product.map((product, index) => (
+            <Card
+              price={product.sellingStatus[0].currentPrice[0].__value__}
+              image={product.galleryURL[0]}
+              title={product.title}
+              location={product.location[0]}
+              shipping={product.shippingInfo[0].shippingType[0]}
+              link={product.viewItemURL[0]}
+              country={product.country}
+              id={product.itemId}
+            />
+          ))
+        )}
       </div>
     );
   }
