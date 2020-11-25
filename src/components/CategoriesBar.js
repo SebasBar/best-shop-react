86620c91fb78c089e36859f038eb8c87da64e1d9
Leonard <leonard.flachs@gmail.com>
2020-11-25 23:23:12 +0100

    add numberpage start refactor

--- src/components/CategoriesBar.js
+++ src/components/CategoriesBar.js
@@ -1,57 +1,76 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
-import "./CategoriesBar.css";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import './CategoriesBar.css';
 
 const categories = [
-  "technology",
-  "fashion",
-  "beauty",
-  "motors",
-  "collectibles",
-  "industrial",
-  "sports",
-  "home&garden",
-  "favourites",
+	'technology',
+	'fashion',
+	'beauty',
+	'motors',
+	'collectibles',
+	'industrial',
+	'sports',
+	'home&garden',
+	'favourites',
 ];
 
 class CreateCategoriesBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categoryInput: "",
-    };
-  }
-  handleSubmit = (value) => {
-    this.props.history.push(`/${value}`);
-
-    // this.props.onSearch(this.state.categoryInput);
-  };
-  handleChange = (event) => {
-    this.setState({ categoryInput: event });
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="categories-bar">
-          {categories.map((category, index) => (
-            <div className="button-container">
-              <Link key={index} to={`/categories/${category}`}>
-                <button
-                  key={index}
-                  className="button"
-                  value={category}
-                  onClick={(event) => this.handleSubmit(event.target.value)}
-                >
-                  {category}
-                </button>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			categoryInput: '',
+			numItemDisplay: 30,
+		};
+	}
+	handleSubmit = (value) => {
+		this.props.history.push(`/${value}`);
+	};
+	handleChange = (event) => {
+		this.setState({ categoryInput: event });
+	};
+	handleNumProduct = (event) => {
+		this.setState({ numItemDisplay: event.target.value });
+		this.props.history.push(
+			`${this.props.location.pathname}?number=${event.target.value}`
+		);
+	};
+	render() {
+		return (
+			<div>
+				<label>Products per page </label>
+				<select
+					id='myList'
+					value={this.state.numItemDisplay}
+					onChange={this.handleNumProduct}
+				>
+					{[20, 30, 50].map((item, index) => (
+						<option key={index} value={item}>
+							{item}
+						</option>
+					))}
+				</select>
+				<div className='categories-bar'>
+					{categories.map((category, index) => (
+						<div className='button-container'>
+							<Link
+								key={index}
+								to={`/categories/${category}/1?number=${this.props.numberItem}`}
+							>
+								<button
+									key={index}
+									className='button'
+									value={category}
+									onClick={(event) => this.handleSubmit(event.target.value)}
+								>
+									{category}
+								</button>
+							</Link>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withRouter(CreateCategoriesBar);
--- src/components/DisplayProducts.js
+++ src/components/DisplayProducts.js
@@ -1,79 +1,102 @@
-import React from "react";
-import Card from "./Card";
-import queryString from "query-string";
-import fetching from "../utils/fetching.js";
+import React from 'react';
+import Card from './Card';
+import queryString from 'query-string';
+import fetching from '../utils/fetching.js';
+import { Link } from 'react-router-dom';
 
 class DisplayProduct extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      searchResults: [],
-      error: false,
-      numItemDisplay: 30,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			searchResults: [],
+			error: false,
+			numItemDisplay: 30,
+			page: 1,
+		};
+	}
 
-  componentDidMount() {
-    this.handleFetch();
-  }
+	componentDidMount() {
+		this.handleFetch();
+	}
 
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.location?.search !== prevProps.location?.search ||
-      this.props.match?.params.name !== prevProps.match?.params.name
-      // || this.props.numberItem !== prevProps.numberItem
-    ) {
-      this.handleFetch();
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (
+			this.props.location?.search !== prevProps.location?.search ||
+			this.props.match?.params.name !== prevProps.match?.params.name ||
+			this.props.match?.params.page !== prevProps.match?.params.page
+		) {
+			this.handleFetch();
+		}
+	}
 
-  handleFetch() {
-    this.setState({ loading: true });
-    const search = queryString.parse(this.props.location.search);
+	handleFetch() {
+		this.setState({ loading: true });
+		const search = queryString.parse(this.props.location.search);
+		console.log('number', search);
+		console.log('this is props id', this.props);
+		// console.log(this.props.match?.params.name);
+		const query = this.props.match?.params.name || search.name || '';
+		fetching(query, search.number || 30, this.props.match.params.page)
+			.then((data) => {
+				console.log('data', data);
+				this.setState({
+					loading: false,
+					searchResults:
+						data.findItemsByKeywordsResponse[0].searchResult[0].item,
+					paginationOutput: Number(
+						data.findItemsByKeywordsResponse[0].paginationOutput[0]
+							.totalPages[0]
+					),
+				});
+			})
+			.catch((err) => {
+				this.setState({
+					error: err,
+				});
+			});
+	}
 
-    console.log("this is props id", this.props);
-    console.log(this.props.match?.params.name);
-    const query = this.props.match?.params.name || search.name || "";
-    fetching(query, this.props.numItemDisplay)
-      .then((data) =>
-        this.setState({
-          loading: false,
-          searchResults:
-            data.findItemsByKeywordsResponse[0].searchResult[0].item,
-        })
-      )
-      .catch((err) => {
-        this.setState({
-          error: err,
-        });
-      });
-  }
+	displayPagination(totalpages) {
+		const actualPage = Number(this.props.match.params.page);
+		let output = [<Link to={`./${actualPage - 1}`}>Previous</Link>];
+		// 0 - 10 -- 10 - 20
+		console.log(totalpages);
+		for (let i = actualPage; i < actualPage + 10; i++) {
+			output.push(<Link to={`./${i}`}>page {i} </Link>);
+		}
+		output.push(<Link to={`./${actualPage + 11}`}>Next</Link>);
+		return output;
+	}
 
-  render() {
-    console.log(this.state);
+	render() {
+		console.log(this.state);
 
-    return this.state.loading ? (
-      <h1>Loading...</h1>
-    ) : (
-      this.state.searchResults
-        .slice(this.props.numberItem)
-        .map((product, index) => (
-          <Card
-            price={product.sellingStatus[0].currentPrice[0].__value__}
-            image={product.galleryURL[0]}
-            title={product.title[0]}
-            location={product.location[0]}
-            shipping={product.shippingInfo[0].shippingType[0]}
-            link={product.viewItemURL[0]}
-            country={product.country[0]}
-            id={product.itemId[0]}
-            addFav={this.props.addFav}
-            favourite={this.props.favourite}
-          />
-        ))
-    );
-  }
+		return this.state.loading ? (
+			<h1>Loading...</h1>
+		) : (
+			<>
+				{this.state.searchResults
+					// slice will reduce the array from 0 to x (x = props.numberItem)
+					.slice(0, this.props.numberItem)
+					.map((product, index) => (
+						<Card
+							price={product.sellingStatus[0].currentPrice[0].__value__}
+							image={product.galleryURL[0]}
+							title={product.title[0]}
+							location={product.location[0]}
+							shipping={product.shippingInfo[0].shippingType[0]}
+							link={product.viewItemURL[0]}
+							country={product.country[0]}
+							id={product.itemId[0]}
+							addFav={this.props.addFav}
+							favourite={this.props.favourite}
+						/>
+					))}
+				{this.displayPagination(this.state.paginationOutput)}
+			</>
+		);
+	}
 }
 
 export default DisplayProduct;
